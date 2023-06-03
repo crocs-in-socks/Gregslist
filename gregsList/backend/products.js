31e33fb30d73e9d61383cd7058004e2532e5cd57
crocs-in-socks <gouriswag1@gmail.com>
2023-06-04 05:13:24 +0530

    added product descriptions

--- gregsList/backend/products.js
+++ gregsList/backend/products.js
@@ -2,57 +2,64 @@ const products = [
     { 
         id:1,
         name: "Rodeo motpro",
-        desc: "desc1",
+        desc: "It's actually called Rodeo motopro",
         price: 1999,
         image: "https://cdn.pixabay.com/photo/2013/07/13/13/43/racing-bicycle-161449_1280.png"
     },
     {
         id: 2,
         name: "Encyclopedia set",
-        desc: "desc2",
+        desc: "The encyclopedia for all questions you didn't ask",
         price: 999,
         image: "https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_640.jpg"
     },
     {
         id: 3,
-        name: "4L Bucket",
-        desc: "desc3",
+        name: "12L Bucket",
+        desc: "Extra Large",
         price: 299,
         image: "https://cdn.pixabay.com/photo/2016/09/04/07/02/bucket-1643406_1280.png"
     },
     {
         id: 4,
         name: "Mattress",
-        desc: "desc4",
+        desc: "Mattress in distress due to his Mistress",
         price: 2999,
         image: "https://cdn.pixabay.com/photo/2017/02/01/09/43/mattress-2029190_1280.jpg"
     },
     {
         id: 5,
         name: "Dumbell",
-        desc: "desc5",
+        desc: "Why not smartbell? He's trying his best",
         price: 4000,
         image: "https://cdn.pixabay.com/photo/2018/05/28/12/53/dumbell-3435964_1280.jpg"
     },
     {
         id: 6,
         name: "Calculator",
-        desc: "desc6",
+        desc: "Permanent divide by zero error",
         price: 2000,
         image: "https://cdn.pixabay.com/photo/2014/07/06/13/55/calculator-385506_1280.jpg"
     },
     {
         id: 7,
         name: "Mobile Phone",
-        desc: "desc7",
+        desc: "real iphon 100% not fake",
         price: 69420,
         image: "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_1280.jpg"
     },
     {
         id: 8,
         name: "Chips",
-        desc: "desc8",
+        desc: "You're not you when you're hungry. Have a Snickers.",
         price: 2,
+        image: "https://cdn.pixabay.com/photo/2020/10/15/18/18/chips-5657633_1280.jpg"
+    },
+    {
+        id: 9,
+        name: "Discounted Chips",
+        desc: "(Only Slightly Used)",
+        price: 3 ,
         image: "https://cdn.pixabay.com/photo/2020/10/15/18/18/chips-5657633_1280.jpg"
     }
 ]
