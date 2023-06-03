d2a1f84c7bd2c6e2077beb71e5dff0b5348e6bff
sharktornado1 <114720158+sharktornado1@users.noreply.github.com>
2023-06-04 04:01:06 +0530

    more products and scrolling fix

--- gregsList/backend/products.js
+++ gregsList/backend/products.js
@@ -26,6 +26,34 @@ const products = [
         desc: "desc4",
         price: 2999,
         image: "https://cdn.pixabay.com/photo/2017/02/01/09/43/mattress-2029190_1280.jpg"
+    },
+    {
+        id: 5,
+        name: "Dumbell",
+        desc: "desc5",
+        price: 4000,
+        image: "https://cdn.pixabay.com/photo/2018/05/28/12/53/dumbell-3435964_1280.jpg"
+    },
+    {
+        id: 6,
+        name: "Calculator",
+        desc: "desc6",
+        price: 2000,
+        image: "https://cdn.pixabay.com/photo/2014/07/06/13/55/calculator-385506_1280.jpg"
+    },
+    {
+        id: 7,
+        name: "Mobile Phone",
+        desc: "desc7",
+        price: 69420,
+        image: "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_1280.jpg"
+    },
+    {
+        id: 8,
+        name: "Chips",
+        desc: "desc8",
+        price: 2,
+        image: "https://cdn.pixabay.com/photo/2020/10/15/18/18/chips-5657633_1280.jpg"
     }
 ]
 module.exports = products;
